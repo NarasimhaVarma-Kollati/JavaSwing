6e5a24856482697a21cd0e98bc7baa753d1e58af
NarasimhaVarma-Kollati <136837121+NarasimhaVarma-Kollati@users.noreply.github.com>
2023-10-07 12:45:10 +0530

    Add files via upload

---
+++ Frame2.java
@@ -0,0 +1,49 @@
+package p6;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class Frame2 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame2 frame = new Frame2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 757, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("WELCOME");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 27));
+		lblNewLabel.setBounds(229, 122, 244, 84);
+		contentPane.add(lblNewLabel);
+	}
+}
