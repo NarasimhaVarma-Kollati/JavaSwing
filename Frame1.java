e3617d8f7022ddd6d3afca3c86eb1267fc776a48
NarasimhaVarma-Kollati <136837121+NarasimhaVarma-Kollati@users.noreply.github.com>
2023-10-07 12:44:51 +0530

    Add files via upload

---
+++ Frame1.java
@@ -0,0 +1,57 @@
+package p6;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Frame1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame1 frame = new Frame1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 675, 394);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Click here!!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Frame2 obj=new Frame2();
+				obj.setVisible(true);
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 22));
+		btnNewButton.setBounds(191, 120, 201, 72);
+		contentPane.add(btnNewButton);
+	}
+}
